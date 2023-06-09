bfa83321959d9bd2c196661c05d0b02123cc7ea6
Billy <38408756+BillyCurtis@users.noreply.github.com>
2023-06-09 04:31:36 +0100

    Add files via upload

---
+++ OpenDiscordSafariExtension.appex/content.js
@@ -0,0 +1,23 @@
+browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
+    console.log("Received response: ", response);
+});
+
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log("Received request: ", request);
+});
+
+function afterNavigate() {
+    var locationArr = window.location.pathname.split("/")
+    if (locationArr.includes("invite") || locationArr.includes("channels")) {
+        window.location.href = `com.hammerandchisel.discord://discord.com${window.location.pathname}`
+    }
+}
+
+(document.body || document.documentElement).addEventListener('transitionend',
+  function(/*TransitionEvent*/ event) {
+    if (event.propertyName === 'width' && event.target.id === 'progress') {
+        afterNavigate();
+    }
+}, true);
+// After page load
+afterNavigate();
